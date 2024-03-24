d62d5c6ed5a92574679fc61a6ca613ac7d660f96
abdulsamad <alogbaabdulsamad@gmail.com>
2024-03-24 18:25:53 -0500

    hhh

---
+++ HTML-main/index.js
@@ -0,0 +1,30 @@
+var body = document.getElementById("body");
+var isactivated = false;
+var wallpaperbutton = document.getElementById("wallpaperbutton");
+var wallpaperdiv = document.getElementById("wallpaperdiv");
+
+button1.onclick = function(){
+    body.style.backgroundImage = "url('/wallpaperlist/defualt.jpg')";
+}
+button2.onclick = function(){
+  body.style.backgroundImage = "url('/wallpaperlist/wallpaper1.jpg')";
+}
+button3.onclick = function(){
+    body.style.backgroundImage = "url('/wallpaperlist/wallpaper2.jpg')";
+  }
+button4.onclick = function(){
+    body.style.backgroundImage = "url('/wallpaperlist/wallpaper3.jpg')";
+}
+
+function sus(){
+    if(isactivated === false){
+        isactivated = true;
+        wallpaperdiv.style.opacity = 1;
+        wallpaperdiv.style.pointerEvents = "all";
+    }
+    else if(isactivated === true){
+        isactivated = false;
+        wallpaperdiv.style.opacity = 0;
+        wallpaperdiv.style.pointerEvents = "none";
+    } 
+}
