fa9a02e88a329615b55ce62d4d310276a0f5f2d6
abdulsamad <alogbaabdulsamad@gmail.com>
2024-04-15 21:01:44 -0500

    asd

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ var body = document.getElementById("body");
 var isactivated = false;
 var wallpaperbutton = document.getElementById("wallpaperbutton");
 var wallpaperdiv = document.getElementById("wallpaperdiv");
+var labelerr = document.getElementById("label")
 
 button1.onclick = function(){
     body.style.backgroundImage = "url('/wallpaperlist/defualt.jpg')";
+    labelerr.src = '/wallpaperthumbnails/1wallpaper.png'
+  
 }
 button2.onclick = function(){
   body.style.backgroundImage = "url('/wallpaperlist/wallpaper1.jpg')";
+  labelerr.src = '/wallpaperthumbnails/2wallpaper.png'
 }
 button3.onclick = function(){
     body.style.backgroundImage = "url('/wallpaperlist/wallpaper2.jpg')";
+    labelerr.src = '/wallpaperthumbnails/3wallpaper.png'
   }
-button4.onclick = function(){
-    body.style.backgroundImage = "url('/wallpaperlist/wallpaper3.jpg')";
-}
 
 function sus(){
     if(isactivated === false){
