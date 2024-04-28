2086fc6c4708cdd7059ca4132b4598bd396948ac
abdulsamad <alogbaabdulsamad@gmail.com>
2024-04-28 07:56:39 -0500

    asdadsf

---
+++ index3.js
@@ -0,0 +1,227 @@
+var thedis = document.getElementById("gamedis");
+var thedistext = document.getElementById("gamedisplaydivtext");
+var iframecontent = document.getElementById("iframecont")
+var clock = document.getElementById("clock")
+var closebutton = document.getElementById("exit")
+
+buttonminute.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "10 Minutes Till Dawn";
+  iframecontent.src = "/gameslist/10minutestilldawn/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+button2048.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "2048";
+  iframecontent.src = "/gameslist/2048/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonworld.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "A Small World Cup";
+  iframecontent.src = "/gameslist/asmallworldcup/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttontanks.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Awsome Tanks 2";
+  iframecontent.src = "/gameslist/awsometanks2/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonbros.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Basket Bros";
+  iframecontent.src = "/gameslist/basketbros/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonbasket.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Basket Random";
+  iframecontent.src = "/gameslist/basketrandom/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonboxing.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Boxing Random";
+  iframecontent.src = "/gameslist/boxingrandom/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttoncube.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Cube Field";
+  iframecontent.src = "/gameslist/cubefield/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttoncrossy.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Crossy Road";
+  iframecontent.src = "/gameslist/crossyroads/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttondraw.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Draw The Hill";
+  iframecontent.src = "/gameslist/drawthehill/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttondrift.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Drift Boss";
+  iframecontent.src = "/gameslist/driftboss/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttondrive.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Drive Mad";
+  iframecontent.src = "/gameslist/drivemad/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonfluid.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Fluid Simulator";
+  iframecontent.src = "/gameslist/fluidsim/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttongun2.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Gun Mayhem 2";
+  iframecontent.src = "/gameslist/gunmayhem2/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttongunredux.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Gun Mayhem Redux";
+  iframecontent.src = "/gameslist/gunmayhemredux/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonmonkey.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Monkey Mart";
+  iframecontent.src = "/gameslist/monkeymart/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonparkour.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Noob Steve Parkour";
+  iframecontent.src = "/gameslist/noobsteveparkour/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonpaper.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Paper.io";
+  iframecontent.src = "/gameslist/paperio/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonpapery.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Papery Planes";
+  iframecontent.src = "/gameslist/paperyplanes/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonradius.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Radius Raid";
+  iframecontent.src = "/gameslist/radiusraid/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonsoccer.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Soccer Random";
+  iframecontent.src = "/gameslist/soccerrandom/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonvex.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Vex 6";
+  iframecontent.src = "/gameslist/vex6/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonsubway.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Subway Surfers";
+  iframecontent.src = "/gameslist/subwaysurfers/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+buttonvolley.onclick = function(){
+  thedis.style.opacity = 1;
+  thedistext.textContent = "Volley Random";
+  iframecontent.src = "/gameslist/volleyrandom/index.html";
+  clock.style.opacity = 0;
+  document.getElementById("myDiv").style.opacity = 0;
+  document.getElementById("myDiv").style.pointerEvents = "none";
+}
+
+
+closebutton.onclick = function(){
+  thedis.style.opacity = 0;
+  iframecontent.src = "";
+}
